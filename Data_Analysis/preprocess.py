e60dc95ffb05d73b3a0177324f25ae37d7e175f3
Asthanaji05 <shashank.asthana05@gmail.com>
2025-01-30 14:55:21 +0530

    added handling of missing values

--- Data_Analysis/preprocess.py
+++ Data_Analysis/preprocess.py
@@ -4,12 +4,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import sys
+import csv
 # Add the parent directory to sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from ML.common import read_csv
-# use data/diabetes.csv
-df = read_csv("data", "diabetes.csv")
-print(df.head())
+
 
 
 def plot_histogram(df, col):
@@ -78,14 +77,28 @@ def apply_smote(data_path, target_column, test_size=0.2, random_state=42):
 
     # Print class distribution after SMOTE
     print("After SMOTE:", Counter(y_train_resampled))
+    
 
     return X_train_resampled, X_test, y_train_resampled, y_test
 
-# data_path = "data/diabetes.csv"  # Update with the correct path
-# target_column = "Outcome"  # Update with the correct target column name
-# X_train, X_test, y_train, y_test = apply_smote(data_path, target_column)
+def smote(data_path, col):
+    X_train, X_test, y_train, y_test = apply_smote(data_path, col)
+    # Combine X and y into one DataFrame
+    train_df = pd.DataFrame(X_train)
+    train_df[col] = y_train  # Add target column
 
-# function for correlation among variables
+    test_df = pd.DataFrame(X_test)
+    test_df[col] = y_test  # Add target column
+
+    # Save the new datasets
+    train_df.to_csv("data/train_smote.csv", index=False)
+    test_df.to_csv("data/test.csv", index=False)
+    
+def visualize_smote(before,after):
+    bf = pd.read_csv(before)
+    af = pd.read_csv(after)
+    bf_plt = coorelation(bf)
+    af_plt = coorelation(af)
 
 def coorelation(df):
     corr = df.corr()
@@ -94,11 +107,51 @@ def coorelation(df):
     sns.heatmap(corr, annot=True, cmap='coolwarm', square=True)
     plt.plot()
     plt.show()
+    return plt
     # print(corr)
     
-def stat(df,col):
-    # mean , mode , min , max 
-    print(df[col].mean())
+def stats(df,filepath):
+    df.describe().to_csv(filepath)  
+    print('sucess')  
+    return    
+def avgs(df, col):
+    # avgs of col where outcome is 0
+    df_0 = df[df['Outcome'] == 0]
+    avg_0 = df_0[col].mean()
+    # avgs of col where outcome is 1
+    df_1 = df[df['Outcome'] == 1]
+    avg_1 = df_1[col].mean()
+    return avg_0,avg_1
+    
+def handle_missing(df, filepath):
+    Columns= ["Glucose","BloodPressure","SkinThickness","Insulin","BMI" ]
+    for col in Columns:
+        avg0 , avg1 = avgs(df , col)
+        # fill avgs in place of zeros as per [outcome]
+        df.loc[(df['Outcome'] == 0) & (df[col] == 0), col] = avg0
+        df.loc[(df['Outcome'] == 1) & (df[col] == 0), col] = avg1
+    df.to_csv(filepath, index=False)  # File save karega given path pe
+    print("Done !")
+    return df
+
+def check_null(df):
+        # Check for missing values
+    print(df.isnull().sum())
+    # check for zeros in col except outcome
+    for col in df.columns:
+        if col != 'outcome':
+            print(f"Column: {col} :  {df[col].eq(0).sum()}")
+    
+def classfier(df,col):   
+    mean = df[col].mean() # mean
+    # standard deviation
+    std = df[col].std()
+    # classifies as "low" , "medium" , "high" based on mean and std
+    classification = df[col].apply(lambda x : 'High' if x> mean+std else 'low' if x<mean-std else 'Medium')
+    print(classification.value_counts().to_dict())
+    
+
+    
     
     
 
